e6b9d38ce8874b14f707276c0c1ab021f231a7e6
kho_ja <azimxojayevabdulazizxoja@gmail.com>
2024-06-23 16:35:26 +0500

    fixed ligh dark mode problem

--- script.js
+++ script.js
@@ -10,19 +10,25 @@ let dark_icon = document.querySelector(".dark_icon");
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   light_icon.style.display = "none";
   dark_icon.style.display = "block";
+  document.documentElement.style.colorScheme = "dark";
+  document.documentElement.dataset.theme = "dark";
 } else {
   dark_icon.style.display = "none";
   light_icon.style.display = "block";
+  document.documentElement.style.colorScheme = "light";
+  document.documentElement.dataset.theme = "light";
 }
 
 light_icon.onclick = () => {
   document.documentElement.style.colorScheme = "dark";
+  document.documentElement.dataset.theme = "dark";
   light_icon.style.display = "none";
   dark_icon.style.display = "block";
 };
 
 dark_icon.onclick = () => {
   document.documentElement.style.colorScheme = "light";
+  document.documentElement.dataset.theme = "light";
   dark_icon.style.display = "none";
   light_icon.style.display = "block";
 };
