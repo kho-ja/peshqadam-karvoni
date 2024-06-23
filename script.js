5313d53bbc10ce837083d48498fe204db7f51715
kho_ja <azimxojayevabdulazizxoja@gmail.com>
2024-06-23 15:56:07 +0500

    fixed two icon problem

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ hamburger.onclick = () => {
 let light_icon = document.querySelector(".light_icon");
 let dark_icon = document.querySelector(".dark_icon");
 
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  light_icon.style.display = "none";
+  dark_icon.style.display = "block";
+} else {
+  dark_icon.style.display = "none";
+  light_icon.style.display = "block";
+}
+
 light_icon.onclick = () => {
   document.documentElement.style.colorScheme = "dark";
   light_icon.style.display = "none";
