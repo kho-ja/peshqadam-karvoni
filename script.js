554d667a09b6c2604d00b61cce0a4717377432bf
kho_ja <azimxojayevabdulazizxoja@gmail.com>
2024-06-23 15:53:34 +0500

    light-dark modes, and navbar fixed

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const images = document.querySelector("#images");
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("nav .menu");
+hamburger.onclick = () => {
+  menu.classList.toggle("closed");
+};
 
-console.log(images.children.length);
+let light_icon = document.querySelector(".light_icon");
+let dark_icon = document.querySelector(".dark_icon");
+
+light_icon.onclick = () => {
+  document.documentElement.style.colorScheme = "dark";
+  light_icon.style.display = "none";
+  dark_icon.style.display = "block";
+};
+
+dark_icon.onclick = () => {
+  document.documentElement.style.colorScheme = "light";
+  dark_icon.style.display = "none";
+  light_icon.style.display = "block";
+};
